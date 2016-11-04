84340d14bffacb8419d742df3815f6577237c933
Kumar Sannidhya Shukla <feynhat@gmail.com>
2016-11-04 07:08:00 +0530

    first commit

---
+++ run.c
@@ -0,0 +1,109 @@
+/*
+
+	DecaySimulator: runs Monte Carlo simulations of radioactive decay chains
+ 	Copyright (C) 2016, K. Sannidhya Shukla
+
+ 	This file is part of DecaySimulator.
+
+    DecaySimulator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DecaySimulator is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DecaySimulator.  If not, see <http://www.gnu.org/licenses/>.
+    
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define DEFAULT_ITER 100000
+#define DEFAULT_MOL  100000
+
+unsigned rand2(unsigned int);
+
+int main(int argc, char **argv)
+{
+	unsigned i, j, cell, k1, k2, n_mol = DEFAULT_MOL, n_iter = DEFAULT_ITER;
+	bool *state_A, *state_B, *state_C;
+	unsigned (*plot)[3], n_A, n_B, n_C;
+	
+	if (argc < 3) {
+		fprintf(stderr, "usage: run k1 k2 [n_iter] [n_mol]\n");
+		return 1;
+	}
+	k1 = atoi(argv[1]);
+	k2 = atoi(argv[2]);
+	if (argc > 3) {
+		n_iter = atoi(argv[3]);
+	}
+	if (argc > 4) {
+		n_mol = atoi(argv[4]);
+	}
+
+	n_A = n_mol;
+	n_B = n_C = 0;
+	
+	state_A = malloc((sizeof(bool))*n_mol);
+	state_B = malloc((sizeof(bool))*n_mol);
+	state_C = malloc((sizeof(bool))*n_mol);
+	for (i = 0; i < n_mol; i++) {
+		state_A[i] = true;
+		state_B[i] = state_C[i] = false;
+	}
+
+	plot = malloc((sizeof(int)) * (n_iter) * 3);
+	plot[0][0] = n_A;
+	plot[0][1] = n_B;
+	plot[0][2] = n_C;
+
+	for (i = 0; i < n_iter; i++) {
+		for (j = 0; j < k1; j++) {
+			cell = rand2(n_mol);
+			if (cell >= n_mol) {
+				printf("error: rand2 has gone berserk");
+				exit(EXIT_FAILURE);
+			}
+			if (state_A[cell]) {
+				state_A[cell] = false;
+				state_B[cell] = true;
+				n_A--;
+				n_B++;
+			}
+		}
+		for (j = 0; j < k2; j++) {
+			cell = rand2(n_mol);
+			if (state_B[cell]) {
+				state_B[cell] = false;
+				state_C[cell] = true;
+				n_B--;
+				n_C++;
+			}
+		}
+		plot[i+1][0] = n_A;
+		plot[i+1][1] = n_B;
+		plot[i+1][2] = n_C;
+	}
+
+	printf("# t\tn_A\tn_B\tn_C\n");
+	for (i = 0; i < n_iter + 1; i++) {
+		printf("%u\t%u\t%u\t%u\n", i, plot[i][0], plot[i][1], plot[i][2]);
+	}
+
+	return 0;
+}
+
+unsigned rand2(unsigned n)
+{
+	unsigned r;
+	while ((r = (rand() / (RAND_MAX / n))) >= n)
+		;
+	return r;
+}
---
+++ simulate.sh
@@ -0,0 +1,74 @@
+#
+# 	DecaySimulator: runs Monte Carlo simulations of radioactive decay chains
+#	Copyright (C) 2016, K. Sannidhya Shukla
+#
+#	This file is part of DecaySimulator.
+#
+#	DecaySimulator is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   DecaySimulator is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with DecaySimulator.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+printf "DecaySimulator  Copyright (C) 2016  K. Sannidhy Shukla\n"
+printf "This program comes with ABSOLUTELY NO WARRANTY.\n"
+printf "This is free software, and you are welcome to redistribute it under certain conditions; see <https://www.gnu.org/licenses/gpl.html> for details.\n\n"
+
+echo -n "Enter the decay constant for A: "
+read K1
+if [[ -z "${K1// }" ]]; then
+	echo "error: invalid value"
+	exit
+fi
+echo -n "Enter the decay constant for B: "
+read K2
+if [[ -z "${K2// }" ]]; then
+	echo "error: invalid value"
+	exit
+fi
+echo -n "Enter the number of iterations to run (default 100000): "
+read N_ITER
+echo -n "Enter the initial number of molecules of A (default 100000): "
+read N_MOL
+echo -n "Enter the name of the data file: (default plot.dat)"
+read PLOTFILE
+echo -n "Enter the name of output file (default MC_Plot.png): "
+read OUTFILE
+
+if [[ -z "${N_ITER// }" ]]; then
+	N_ITER=100000
+fi
+if [[ -z "${N_MOL// }" ]]; then
+	N_MOL=100000
+fi
+if [[ -z "${PLOTFILE// }" ]]; then
+	PLOTFILE="plot.dat"
+fi
+if [[ -z "${OUTFILE// }" ]]; then
+	OUTFILE="MC_Plot.png"
+fi
+
+cc run.c -o run
+./run "$K1" "$K2" "$N_ITER" "$N_MOL" > "$PLOTFILE"
+
+echo "set term png" > gpscript.p
+echo "set xlabel \"No. of iterations\"" >> gpscript.p
+echo "set ylabel \"No. of molecules\"" >> gpscript.p
+echo "set output \"$OUTFILE\"" >> gpscript.p
+echo "set title \"Chain Reaction A -> B -> C (k1=$K1, k2=$K2)\"" >> gpscript.p
+echo "set xrange [-10:$N_ITER]" >> gpscript.p
+echo "set yrange [-10:$N_MOL]" >> gpscript.p
+echo "plot \
+\"plot.dat\" u 1:2 w lines t \"n(A)\",\
+\"plot.dat\" u 1:3 w lines t \"n(B)\",\
+\"plot.dat\" u 1:4 w lines t \"n(C)\"" >> gpscript.p
+
+gnuplot gpscript.p
